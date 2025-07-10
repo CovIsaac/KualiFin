5300ecd66a10abdf20a84dd2863b5d0f9ecfd148
RicardoJuarez305951 <a305951@alumnos.uaslp.mx>
2025-07-10 11:59:54 -0600

    Seeder recuperado

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'rol', // Default role for the user
+        'telefono', // User's phone number
     ];
 
     /**
